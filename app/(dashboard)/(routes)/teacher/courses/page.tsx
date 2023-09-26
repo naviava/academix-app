0e7cb7d375bd7ab983f881c1031b8ad44d9b5f93
Navin Avadhani <knavinavadhani@gmail.com>
2023-09-26 16:49:43 +0530

    Added course creation UI.

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,5 +1,14 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
 interface CoursesPageProps {}
 
 export default function CoursesPage({}: CoursesPageProps) {
-  return <div>CoursesPage</div>;
+  return (
+    <div className="p-6">
+      <Link href="/teacher/create">
+        <Button>New Course</Button>
+      </Link>
+    </div>
+  );
 }
---
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import { useCallback, useMemo } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+import * as z from "zod";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = z.object({
+  title: z.string().min(1, { message: "Title is required" }),
+});
+
+export default function CreatePage() {
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { title: "" },
+  });
+
+  const { isSubmitting, isValid } = useMemo(
+    () => form.formState,
+    [form.formState],
+  );
+
+  const onSubmit = useCallback(
+    async (values: z.infer<typeof formSchema>) => {
+      try {
+        const response = await axios.post("/api/course", values);
+        router.push(`/teacher/courses/${response.data.id}`);
+      } catch (err) {
+        toast.error("Something went wrong");
+      }
+    },
+    [router],
+  );
+
+  return (
+    <div className="mx-auto flex h-full max-w-5xl p-6 md:items-center md:justify-center">
+      <div>
+        <h1 className="text-2xl">Name your course</h1>
+        <p className="text-sm text-slate-600">
+          What would you like to name the course? Don&apos;t worry, you can
+          change this later.
+        </p>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="mt-8 space-y-8"
+          >
+            <FormField
+              name="title"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Course Title</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="e.g. Web development bootcamp"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    What will you teach in this course?
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Link href="/">
+                <Button type="button" variant="ghost">
+                  Cancel
+                </Button>
+              </Link>
+              <Button type="submit" disabled={!isValid || isSubmitting}>
+                Continue
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+}
