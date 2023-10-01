c751fee51dc0f6a8c39f29725a27e57c1434a10e
Navin Avadhani <knavinavadhani@gmail.com>
2023-10-01 21:50:48 +0530

    Added Chapter access form.

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-access-form.tsx
@@ -0,0 +1,142 @@
+"use client";
+
+import { memo, useCallback, useMemo, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import * as z from "zod";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Pencil } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Chapter } from "@prisma/client";
+
+import Preview from "@/components/preview";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { cn } from "@/lib/utils";
+
+interface ChapterAccessFormProps {
+  initialData: Chapter;
+  courseId: string;
+  chapterId: string;
+}
+
+const formSchema = z.object({
+  isFree: z.boolean().default(false),
+});
+
+function ChapterAccessForm({
+  initialData,
+  courseId,
+  chapterId,
+}: ChapterAccessFormProps) {
+  const router = useRouter();
+  const [isEditing, setIsEditing] = useState(false);
+
+  const toggleEdit = useCallback(() => setIsEditing((prev) => !prev), []);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { isFree: !!initialData.isFree },
+  });
+
+  const { isSubmitting, isValid } = useMemo(
+    () => form.formState,
+    [form.formState],
+  );
+
+  const onSubmit = useCallback(
+    async (values: z.infer<typeof formSchema>) => {
+      try {
+        await axios.patch(
+          `/api/courses/${courseId}/chapters/${chapterId}`,
+          values,
+        );
+        toast.success("Chapter updated");
+        toggleEdit();
+        router.refresh();
+      } catch (err) {
+        toast.error("Something went wrong");
+      }
+    },
+    [courseId, router, chapterId, toggleEdit],
+  );
+
+  return (
+    <div className="mt-6 rounded-md border bg-slate-100 p-4">
+      <div className="flex items-center justify-between font-medium">
+        Chapter access
+        <Button variant="ghost" onClick={toggleEdit}>
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil className="mr-2 h-4 w-4" />
+              Edit access
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        <div
+          className={cn(
+            "mt-2 text-sm",
+            !initialData.isFree && "italic text-slate-500",
+          )}
+        >
+          {initialData?.isFree ? (
+            <>This chapter is free to preview.</>
+          ) : (
+            <>This chapter is NOT free.</>
+          )}
+        </div>
+      )}
+      {isEditing && (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="mt-4 space-y-4"
+          >
+            <FormField
+              name="isFree"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
+                  <FormControl>
+                    <Checkbox
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </FormControl>
+                  <div className="space-y-1 leading-none">
+                    <FormDescription>
+                      Check the box, if this chapter is free to preview.
+                    </FormDescription>
+                  </div>
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Button type="submit" disabled={!isValid || isSubmitting}>
+                Save
+              </Button>
+            </div>
+          </form>
+        </Form>
+      )}
+    </div>
+  );
+}
+
+export default memo(ChapterAccessForm);
