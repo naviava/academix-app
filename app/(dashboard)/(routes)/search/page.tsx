688429ab7b56993a67ccdf1a4a556497f0d805bd
Navin Avadhani <knavinavadhani@gmail.com>
2023-10-02 18:28:39 +0530

    Added course card on browse page.

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,20 +1,36 @@
+import { redirect } from "next/navigation";
+
+import { auth } from "@clerk/nextjs";
+
+import CourseList from "@/components/course-list";
 import SearchInput from "@/components/search-input";
 import Categories from "./_components/categories";
 
 import { db } from "@/lib/db";
+import { getCourses } from "@/actions/get-courses";
 
-export default async function SearchPage() {
+interface SearchPageProps {
+  searchParams: { title: string; categoryId: string };
+}
+
+export default async function SearchPage({ searchParams }: SearchPageProps) {
+  const { userId } = auth();
+  if (!userId) return redirect("/");
+
   const categories = await db.category.findMany({
     orderBy: { name: "asc" },
   });
+
+  const courses = await getCourses({ userId, ...searchParams });
 
   return (
     <>
       <div className="block px-6 pt-6 md:mb-0 md:hidden">
         <SearchInput />
       </div>
-      <div className="p-6">
+      <div className="space-y-4 p-6">
         <Categories items={categories} />
+        <CourseList items={courses} />
       </div>
     </>
   );
---
+++ components/course-card.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import { IconBadge } from "@/components/icon-badge";
+import { BookOpen } from "lucide-react";
+import { formatPrice } from "@/lib/format";
+
+interface CourseCardProps {
+  id: string;
+  title: string;
+  imageUrl: string;
+  chaptersLength: number;
+  price: number;
+  progress: number | null;
+  category: string;
+}
+
+export default function CourseCard({
+  id,
+  title,
+  imageUrl,
+  chaptersLength,
+  price,
+  progress,
+  category,
+}: CourseCardProps) {
+  return (
+    <Link href={`/courses/${id}`}>
+      <div className="group h-full overflow-hidden rounded-lg border p-3 transition hover:shadow-sm">
+        <div className="relative aspect-video w-full overflow-hidden rounded-md">
+          <Image fill className="object-cover" alt={title} src={imageUrl} />
+        </div>
+        <div className="flex flex-col pt-2">
+          <div className="line-clamp-2 text-lg font-medium transition group-hover:text-sky-700 md:text-base">
+            {title}
+          </div>
+          <p className="text-xs text-muted-foreground">{category}</p>
+          <div className="my-3 flex items-center gap-x-2 text-sm md:text-xs">
+            <div className="flex items-center gap-x-1 text-slate-500">
+              <IconBadge size="sm" icon={BookOpen} />
+              <span>
+                {chaptersLength} {chaptersLength === 1 ? "chapter" : "chapters"}
+              </span>
+            </div>
+          </div>
+          {progress !== null ? (
+            <div>TODO: Progress component</div>
+          ) : (
+            <p className="text-md font-medium text-slate-700 md:text-sm">
+              {formatPrice(price)}
+            </p>
+          )}
+        </div>
+      </div>
+    </Link>
+  );
+}
